fc4eb62326cbe7586fd3a6c6f004e8a777b35e2f
u <u@pop-os.localdomain>
2024-04-13 00:43:44 -0300

    recreating index

---
+++ listeners.js
@@ -0,0 +1,101 @@
+import { updateMap, resetMap } from "./script.js";
+
+const dateInput = document.getElementById("dateInput");
+const dateSlider = document.getElementById("dateSlider");
+
+let selectedJson = "mars";
+let systemData = await getSystemData(selectedJson);
+
+dateSlider.addEventListener("input", function () {
+  const sliderValue = parseInt(dateSlider.value);
+  const diaInicio = new Date(2224, 5, 1);
+  const selectedDate = new Date(
+    diaInicio.getTime() + sliderValue * 7 * 24 * 60 * 60 * 1000
+  );
+  dateInput.value = `${selectedDate.getDate()}/${
+    selectedDate.getMonth() + 1
+  }/${selectedDate.getFullYear()}`;
+
+  const event = new Event("change");
+  dateInput.dispatchEvent(event);
+
+  updatePositions(sliderValue);
+
+});
+
+dateInput.addEventListener("change", function () {
+  const selectedDate = dateInput.value + "";
+  updatePositions(selectedDate);
+});
+
+document.getElementById("saveImage").addEventListener("click", saveImage);
+
+export async function getSystemData(selectedJson) {
+  let systemData = {};
+
+  await fetch(`../json/${selectedJson}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      systemData = data;
+    })
+    .catch((error) => {
+      console.error("Erro:", error);
+    });
+
+  return systemData;
+}
+
+export async function updateSystemData(selectedJson) {
+  systemData = await getSystemData(selectedJson);
+}
+
+function parseDate(dateString) {
+  const [day, month, year] = dateString.split("/");
+  return new Date(year, month - 1, day); // month - 1 porque os meses no objeto Date são 0-indexados
+}
+
+function formatDate(dateString) {
+  const selectedDate = parseDate(dateString);
+  const formattedDate = selectedDate.toLocaleDateString("pt-BR").toString();
+  return formattedDate.replaceAll("/", "_");
+}
+
+export async function updatePositions(dateText) {
+  const diaInicio = new Date(2224, 5, 1);
+  const selectedDate = parseDate(dateText);
+  const diasPassados = (selectedDate - diaInicio) / (1000 * 60 * 60 * 24);
+  for (const orbit in systemData.orbits) {
+    const grauDias = systemData.orbits[orbit].grauPorDia;
+    const grau =
+      diasPassados * grauDias < 360
+        ? diasPassados * grauDias
+        : (diasPassados * grauDias) % 360;
+    systemData.orbits[orbit].grau =
+      systemData.orbits[orbit].grauBase + grau * -1;
+  }
+  const formattedDate = selectedDate.toLocaleDateString("pt-BR");
+
+  systemData.date = formattedDate;
+
+  updateMap(systemData);
+}
+
+document.querySelectorAll(".navbar-item").forEach((item) => {
+  item.addEventListener("click", async function () {
+    // Resetar o mapa
+    resetMap();
+
+    // Definir o campo de texto para a data "01/06/2224"
+    const dateInput = document.getElementById("dateInput");
+    dateInput.value = "01/06/2224";
+
+    // Definir o slider para a posição zero
+    const dateSlider = document.getElementById("dateSlider");
+    dateSlider.value = 0;
+
+    // Atualizar os dados do sistema e o mapa
+    selectedJson = this.getAttribute("data-json");
+    await updateSystemData(selectedJson);
+    updateMap(systemData);
+  });
+});
---
+++ script.js
@@ -0,0 +1,175 @@
+import { getSystemData } from "./listeners.js";
+
+const minZoom = -3;
+let currentZoom = minZoom;
+
+var map = (map = L.map("map", {
+  crs: L.CRS.Simple,
+  minZoom: minZoom,
+}));
+
+let bodiesLayer = L.layerGroup().addTo(map);
+let placesLayer = L.layerGroup().addTo(map);
+
+let systemData = await getSystemData("mars");
+
+updateMap(systemData);
+
+export function updateMap(systemData) {
+  map.setView([4320, 4320], minZoom);
+
+  var imageUrl = "../" + systemData.imageUrl;
+
+  var imageBounds = [
+    [0, 0],
+    [8640, 8640],
+  ];
+
+  clearMap(map);
+
+  L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+  map.setMaxBounds(imageBounds);
+  map.setView([4320, 4320], minZoom);
+
+  addBodies(map, systemData);
+  // addPlaces(map, systemData);
+}
+
+export function addBodies(map, systemData) {
+  const orbits = systemData.orbits;
+  const mapCenter = map.latLngToContainerPoint(map.getCenter());
+
+  for (const orbit in orbits) {
+    const orbitData = orbits[orbit];
+    const bodyDiameter = orbitData.diametro * systemData.bodyScale;
+    if (bodyDiameter > 0) {
+      const raioOrbita = (orbitData.diametroOrbita * systemData.scale) / 8;
+      const x =
+        mapCenter.x +
+        raioOrbita * Math.cos((orbitData.grau - 90) * (Math.PI / 180));
+      const y =
+        mapCenter.y +
+        raioOrbita * Math.sin((orbitData.grau - 90) * (Math.PI / 180));
+
+      const latLng = map.containerPointToLatLng(L.point(x, y));
+
+      var html = `
+      <div style="width: ${bodyDiameter}px; height: ${bodyDiameter}px; background-color: ${orbitData.cor}; border-radius: 50%; position: relative;">
+        <div style="position: absolute; top: -50%; left: 50%; transform: translate(-50%, -50%); color: white; font-size: 12px;">
+          ${orbitData.name}
+        </div>
+      </div>`;
+
+      // Criar um ícone personalizado para o marcador
+      const markerIcon = L.divIcon({
+        className: "custom-marker",
+        html: html,
+        iconSize: [bodyDiameter, bodyDiameter],
+        iconAnchor: [bodyDiameter / 2, bodyDiameter / 2],
+      });
+
+      const marker = L.marker(latLng, { icon: markerIcon }).bindPopup(
+        orbitData.name
+      );
+      bodiesLayer.addLayer(marker);
+    }
+  }
+}
+
+function resizeBodiesIcons(map, zoomMultiplier) {
+  map.eachLayer(function (layer) {
+    if (layer instanceof L.Marker && bodiesLayer.hasLayer(layer)) {
+          var icon = layer.options.icon;
+          var iconSize = icon.options.iconSize;
+          if (iconSize) {
+            var newSize = [
+              iconSize[0] * zoomMultiplier,
+              iconSize[1] * zoomMultiplier,
+            ];
+            var newAnchor = [newSize[0] / 2, newSize[1] / 2];
+            icon.options.iconSize = newSize;
+            icon.options.iconAnchor = newAnchor;
+            var popupContent = layer.getPopup()
+              ? layer.getPopup().getContent()
+              : "";
+            layer.setIcon(icon);
+
+            var html = `
+            <div style="width: ${newSize[0]}px; height: ${newSize[1]}px; background-color: ${
+              icon.options.html.match(/background-color:\s*(.*?);/)[1]
+            }; border-radius: 50%; position: relative;">
+              <div style="position: absolute; top: -50%; left: 50%; transform: translate(-50%, -50%); color: white; font-size: 12px;">
+                ${popupContent}
+              </div>
+            </div>`;
+            icon.options.html = html;
+            layer.setIcon(icon);
+          }
+    }
+  });
+}
+
+export function addPlaces(map, systemData) {
+  const orbits = systemData.orbits;
+  const mapCenter = map.latLngToContainerPoint(map.getCenter());
+  const markersData = []; // Array para armazenar os dados dos marcadores
+
+  for (const orbit in orbits) {
+    const orbitData = orbits[orbit];
+    const orbitPlaces = orbitData.places;
+    for (const place in orbitPlaces) {
+      const placeData = orbitPlaces[place];
+
+      const raioOrbita = (orbitData.diametroOrbita * systemData.scale) / 8;
+      const x =
+        mapCenter.x +
+        placeData.xOffset / 8 +
+        raioOrbita * Math.cos((orbitData.grau - 90) * (Math.PI / 180));
+      const y =
+        mapCenter.y +
+        placeData.yOffset / 8 +
+        raioOrbita * Math.sin((orbitData.grau - 90) * (Math.PI / 180));
+
+      const latLng = map.containerPointToLatLng(L.point(x, y));
+
+      const marker = L.marker(latLng).bindPopup(placeData.name);
+      placesLayer.addLayer(marker);
+    }
+  }
+  systemData.markersData = markersData;
+}
+
+export function clearMap() {
+  resetMap();
+}
+
+export function resetMap() {
+  const mapContainer = document.getElementById("mapContainer");
+  mapContainer.innerHTML = ""; // Remove o conteúdo existente
+
+  const newMapDiv = document.createElement("div");
+  newMapDiv.id = "map";
+  mapContainer.appendChild(newMapDiv); // Adiciona um novo div com o id "map"
+
+  map = map = L.map("map", {
+    crs: L.CRS.Simple,
+    minZoom: minZoom,
+  });
+
+  bodiesLayer = L.layerGroup().addTo(map);
+  placesLayer = L.layerGroup().addTo(map);
+
+  // Event listener for zoom end
+  map.on("zoomend", function () {
+    // Calcula o multiplicador com base na diferença de zoom
+    var zoomDifference = map.getZoom() - currentZoom;
+    var zoomMultiplier = Math.pow(1.5, zoomDifference);
+
+    // Atualiza o nível de zoom atual
+    currentZoom = map.getZoom();
+
+    // Redimensiona os ícones dos marcadores
+    resizeBodiesIcons(map, zoomMultiplier);
+  });
+}
